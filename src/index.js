bc4c25ca9b9f522d28a397beca72f4b95883e0eb
Marcha <marchaf@gmail.com>
2022-10-06 10:57:32 +0200

    fix warning react17 vs react18 createRoot

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { render } from 'react-dom';
+import { createRoot} from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
 
 
-const rootElement = document.getElementById('root');
-
-render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
